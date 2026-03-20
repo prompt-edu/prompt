d52ac1837d7115ae34277d750e93d110f6f11e16
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-03-20 18:31:52 +0100

    add init for new storage functions

--- servers/core/main.go
+++ servers/core/main.go
@@ -30,7 +30,8 @@ import (
 	"github.com/prompt-edu/prompt/servers/core/mailing"
 	"github.com/prompt-edu/prompt/servers/core/permissionValidation"
 	"github.com/prompt-edu/prompt/servers/core/privacy"
-	"github.com/prompt-edu/prompt/servers/core/storage"
+	"github.com/prompt-edu/prompt/servers/core/storage/files"
+	"github.com/prompt-edu/prompt/servers/core/storage/privacyexport"
 	"github.com/prompt-edu/prompt/servers/core/student"
 	log "github.com/sirupsen/logrus"
 )
@@ -227,9 +228,12 @@ func main() {
 	applicationAdministration.InitApplicationAdministrationModule(api, *query, conn)
 	instructorNote.InitInstructorNoteModule(api, *query, conn)
 
-	// Initialize storage module (service only)
-	if err := storage.InitStorageModule(*query, conn); err != nil {
-		log.Fatalf("Failed to initialize storage module: %v", err)
+	if err := files.Init(*query, conn); err != nil {
+		log.Fatalf("Failed to initialize prompt file storage: %v", err)
+	}
+
+	if err := privacyexport.Init(); err != nil {
+		log.Fatalf("Failed to initialize privacy export storage: %v", err)
 	}
 
 	privacy.InitPrivacyModule(api, *query, conn)

5bf6a24b3f8ad9182305d8c1aa841f40608c828b
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-03-20 18:30:28 +0100

    privacy export table queries

---
+++ servers/core/db/query/privacy_export.sql
@@ -0,0 +1,24 @@
+-- name: GetExportRecordByID :one
+SELECT * FROM privacy_export WHERE id = $1 LIMIT 1;
+
+-- name: GetExportRecordByIDWithDocs :one
+SELECT * FROM privacy_export_with_docs WHERE id = $1;
+
+-- name: GetLatestExportForUserWithDocs :one
+SELECT * FROM privacy_export_with_docs WHERE userID = $1 ORDER BY date_created DESC LIMIT 1;
+
+-- name: CreateNewExport :one
+INSERT INTO privacy_export ( id, userID, studentID, status )
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+
+-- name: CreateNewExportDoc :one
+INSERT INTO privacy_export_document ( id, exportID, source_name, object_key, status )
+VALUES ($1, $2, $3, $4, $5)
+RETURNING *;
+
+-- name: SetExportStatus :one
+UPDATE privacy_export SET status = $2 WHERE id = $1 RETURNING *;
+
+-- name: SetExportDocStatus :one
+UPDATE privacy_export_document SET status = $2 WHERE id = $1 RETURNING *;

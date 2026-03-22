209ffa3183ae65866cee3a1bb0fef2c49a0ce81f
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-03-22 19:57:35 +0100

    remove old log

--- clients/core/src/managementConsole/pages/SystemStatusPage/network/getServiceCapabilities.ts
+++ clients/core/src/managementConsole/pages/SystemStatusPage/network/getServiceCapabilities.ts
@@ -3,8 +3,6 @@ import { ServiceInfo } from '../interfaces/serviceCapabilities'
 import { CoursePhaseType } from '../interfaces/coursePhaseType'
 
 export const getServiceInfo = async (service: CoursePhaseType): Promise<ServiceInfo> => {
-  console.log('get service info for ' + `${service.baseUrl}/info`)
-
   const response = await axios.get<ServiceInfo>(`${service.baseUrl}/info`)
   return response.data
 }

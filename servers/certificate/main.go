f5ac214edc0a1d1f5d264b332607292442b0ce97
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-03-23 15:36:05 +0100

    use timeout context

--- servers/certificate/main.go
+++ servers/certificate/main.go
@@ -76,7 +76,8 @@ func main() {
 
 	runMigrations(databaseURL)
 
-	conn, err := pgxpool.New(context.Background(), databaseURL)
+  ctx := context.Background()
+	conn, err := pgxpool.New(ctx, databaseURL)
 	if err != nil {
 		log.Fatalf("Unable to create connection pool: %v\n", err)
 		os.Exit(1)
@@ -103,17 +104,20 @@ func main() {
 	generator.InitGeneratorModule(api, *query)
 
 	promptTypes.RegisterInfoEndpoint(apiBase, promptTypes.ServiceInfo{
-		ServiceName: "certificate",
-		Version:     promptSDK.GetEnv("SERVER_IMAGE_TAG", ""),
-		Capabilities: map[string]bool{
-			promptTypes.CapabilityPrivacyExport:   false,
-			promptTypes.CapabilityPrivacyDeletion: false,
-			promptTypes.CapabilityPhaseCopy:       false,
-			promptTypes.CapabilityPhaseConfig:     true,
-		},
-	}, func() bool {
-		return conn.Ping(context.Background()) == nil
-	})
+      ServiceName: "certificate",
+      Version:     promptSDK.GetEnv("SERVER_IMAGE_TAG", ""),
+      Capabilities: map[string]bool{
+        promptTypes.CapabilityPrivacyExport:   false,
+        promptTypes.CapabilityPrivacyDeletion: false,
+        promptTypes.CapabilityPhaseCopy:       false,
+        promptTypes.CapabilityPhaseConfig:     true,
+      },
+    }, func() bool {
+      ctt, cancel := context.WithTimeout(ctx, 500*time.Millisecond)
+      defer cancel()
+      return conn.Ping(ctt) == nil
+    },
+  )
 
 	serverAddress := promptSDK.GetEnv("SERVER_ADDRESS", "localhost:8088")
 	log.Info("Certificate Server started")
@@ -124,6 +128,7 @@ func main() {
 }
 
 func helloCertificate(c *gin.Context) {
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Hello from certificate service",
 	})

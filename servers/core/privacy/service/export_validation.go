f73cd53bc79a51cd879a3ec3b37211141f08deb7
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-03-30 11:36:01 +0200

    gofmt

--- servers/core/privacy/service/export_validation.go
+++ servers/core/privacy/service/export_validation.go
@@ -13,47 +13,47 @@ import (
 )
 
 func ValidateExportValid(c context.Context, exportID uuid.UUID) error {
-  exp, err := PrivacyServiceSingleton.queries.GetExportRecordByID(c, exportID)
-  if err != nil {
-    return err
-  }
-  expDTO := privacyDTO.GetPrivacyExportDTOFromDBModel(exp)
+	exp, err := PrivacyServiceSingleton.queries.GetExportRecordByID(c, exportID)
+	if err != nil {
+		return err
+	}
+	expDTO := privacyDTO.GetPrivacyExportDTOFromDBModel(exp)
 
-  if time.Now().After(expDTO.ValidUntil) {
-    return errors.New("the export is no longer valid")
-  }
-  return nil
+	if time.Now().After(expDTO.ValidUntil) {
+		return errors.New("the export is no longer valid")
+	}
+	return nil
 }
 
 func ValidateExportDocBelongsToExport(c context.Context, docID uuid.UUID, exportID uuid.UUID) error {
-  expWD, err := GetExportWithDocs(c, exportID)
-  if err != nil {
-    return err
-  }
-  for _, expDoc := range expWD.Documents {
-    if expDoc.ID == docID {
-      return nil
-    }
-  }
-  return fmt.Errorf("the export doc with given ID does not belong to the export with given ID")
+	expWD, err := GetExportWithDocs(c, exportID)
+	if err != nil {
+		return err
+	}
+	for _, expDoc := range expWD.Documents {
+		if expDoc.ID == docID {
+			return nil
+		}
+	}
+	return fmt.Errorf("the export doc with given ID does not belong to the export with given ID")
 }
 
 func ValidateExportBelongsToRequester(c *gin.Context, exportID uuid.UUID) error {
-  exp, err := PrivacyServiceSingleton.queries.GetExportRecordByID(c, exportID)
-  if err != nil {
-    return err
-  }
+	exp, err := PrivacyServiceSingleton.queries.GetExportRecordByID(c, exportID)
+	if err != nil {
+		return err
+	}
 
-  expDTO := privacyDTO.GetPrivacyExportDTOFromDBModel(exp)
+	expDTO := privacyDTO.GetPrivacyExportDTOFromDBModel(exp)
 
-  requesterUserID, errGetID := utils.GetUserUUIDFromContext(c)
-  if errGetID != nil {
-    return errGetID
-  }
+	requesterUserID, errGetID := utils.GetUserUUIDFromContext(c)
+	if errGetID != nil {
+		return errGetID
+	}
 
-  if expDTO.UserID != requesterUserID {
-    return errors.New("export does not belong to the requester")
-  }
+	if expDTO.UserID != requesterUserID {
+		return errors.New("export does not belong to the requester")
+	}
 
-  return nil
+	return nil
 }

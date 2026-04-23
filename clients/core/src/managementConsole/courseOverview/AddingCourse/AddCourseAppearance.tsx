e5143c8bfdc0c316c12177e5b67a75383882d0c8
Maximilian Rapp <maximilian.rapp@tum.de>
2026-04-24 00:37:20 +0200

    Fix formatting of color selection items in AddCourseAppearance component

--- clients/core/src/managementConsole/courseOverview/AddingCourse/AddCourseAppearance.tsx
+++ clients/core/src/managementConsole/courseOverview/AddingCourse/AddCourseAppearance.tsx
@@ -73,7 +73,9 @@ export const AddCourseAppearance: React.FC<AddCourseAppearanceProps> = ({
                       {courseAppearanceColors.map((color) => (
                         <SelectItem key={color} value={color}>
                           <div className='flex items-center gap-2'>
-                            <div className={`w-5 h-5 rounded-sm border border-border ${color}`}></div>
+                            <div
+                              className={`w-5 h-5 rounded-sm border border-border ${color}`}
+                            ></div>
                             <span className='capitalize'>{color.split('-')[1]}</span>
                           </div>
                         </SelectItem>

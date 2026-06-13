da3d82fa9fdfe2b4d48267d0ab0beea30d76cfd9
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-06-13 19:19:13 +0200

    introduce util to reduce clutter in redaction code

--- servers/assessment/privacy/redact.go
+++ servers/assessment/privacy/redact.go
@@ -7,68 +7,37 @@ import (
 	db "github.com/prompt-edu/prompt/servers/assessment/db/sqlc"
 )
 
-func GetAllAssessmentsByCourseParticipationIDsNoAuthor(ctx context.Context, q db.Queries, ids []uuid.UUID) ([]db.Assessment, error) {
-	items, err := q.GetAllAssessmentsByCourseParticipationIDs(ctx, ids)
+func fetchAndApplyForEach[T any](ctx context.Context, ids []uuid.UUID, f func(context.Context, []uuid.UUID) ([]T, error), apply func(*T)) ([]T, error) {
+	items, err := f(ctx, ids)
 	if err != nil {
 		return nil, err
 	}
 	for i := range items {
-		items[i].Author = ""
+		apply(&items[i])
 	}
 	return items, nil
+}
+
+func GetAllAssessmentsByCourseParticipationIDsNoAuthor(ctx context.Context, q db.Queries, ids []uuid.UUID) ([]db.Assessment, error) {
+	return fetchAndApplyForEach(ctx, ids, q.GetAllAssessmentsByCourseParticipationIDs, func(assessment *db.Assessment) { assessment.Author = "" })
 }
 
 func GetAllAssessmentCompletionsByCourseParticipationIDsNoAuthor(ctx context.Context, q db.Queries, ids []uuid.UUID) ([]db.AssessmentCompletion, error) {
-	items, err := q.GetAllAssessmentCompletionsByCourseParticipationIDs(ctx, ids)
-	if err != nil {
-		return nil, err
-	}
-	for i := range items {
-		items[i].Author = ""
-	}
-	return items, nil
+	return fetchAndApplyForEach(ctx, ids, q.GetAllAssessmentCompletionsByCourseParticipationIDs, func(assessment *db.AssessmentCompletion) { assessment.Author = "" })
 }
 
 func GetAllEvaluationsByCourseParticipationIDsNoAuthor(ctx context.Context, q db.Queries, ids []uuid.UUID) ([]db.Evaluation, error) {
-	items, err := q.GetAllEvaluationsByCourseParticipationIDs(ctx, ids)
-	if err != nil {
-		return nil, err
-	}
-	for i := range items {
-		items[i].AuthorCourseParticipationID = uuid.Nil
-	}
-	return items, nil
+	return fetchAndApplyForEach(ctx, ids, q.GetAllEvaluationsByCourseParticipationIDs, func(evaluation *db.Evaluation) { evaluation.AuthorCourseParticipationID = uuid.Nil })
 }
 
 func GetAllEvaluationCompletionsByCourseParticipationIDsNoAuthor(ctx context.Context, q db.Queries, ids []uuid.UUID) ([]db.EvaluationCompletion, error) {
-	items, err := q.GetAllEvaluationCompletionsByCourseParticipationIDs(ctx, ids)
-	if err != nil {
-		return nil, err
-	}
-	for i := range items {
-		items[i].AuthorCourseParticipationID = uuid.Nil
-	}
-	return items, nil
+	return fetchAndApplyForEach(ctx, ids, q.GetAllEvaluationCompletionsByCourseParticipationIDs, func(evalCompl *db.EvaluationCompletion) { evalCompl.AuthorCourseParticipationID = uuid.Nil })
 }
 
 func GetAllActionItemsByCourseParticipationIDsNoAuthor(ctx context.Context, q db.Queries, ids []uuid.UUID) ([]db.ActionItem, error) {
-	items, err := q.GetAllActionItemsByCourseParticipationIDs(ctx, ids)
-	if err != nil {
-		return nil, err
-	}
-	for i := range items {
-		items[i].Author = ""
-	}
-	return items, nil
+	return fetchAndApplyForEach(ctx, ids, q.GetAllActionItemsByCourseParticipationIDs, func(actionItem *db.ActionItem) { actionItem.Author = "" })
 }
 
 func GetAllFeedbackItemsByCourseParticipationIDsNoAuthor(ctx context.Context, q db.Queries, ids []uuid.UUID) ([]db.FeedbackItem, error) {
-	items, err := q.GetAllFeedbackItemsByCourseParticipationIDs(ctx, ids)
-	if err != nil {
-		return nil, err
-	}
-	for i := range items {
-		items[i].AuthorCourseParticipationID = uuid.Nil
-	}
-	return items, nil
+	return fetchAndApplyForEach(ctx, ids, q.GetAllFeedbackItemsByCourseParticipationIDs, func(feedbackItem *db.FeedbackItem) { feedbackItem.AuthorCourseParticipationID = uuid.Nil })
 }

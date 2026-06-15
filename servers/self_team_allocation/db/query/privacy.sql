577deab3d569e00dd0dbdc3ea04e51852db02b57
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-06-15 10:23:08 +0200

    add team info to provide more context

--- servers/self_team_allocation/db/query/privacy.sql
+++ servers/self_team_allocation/db/query/privacy.sql
@@ -1,5 +1,16 @@
 -- name: GetAssignmentsByParticipationIDs :many
-SELECT *
-FROM assignments
-WHERE course_participation_id = ANY(@course_participation_ids::uuid[])
-ORDER BY created_at ASC;
+SELECT 
+    a.id,
+    a.course_participation_id,
+    a.course_phase_id,
+    a.student_first_name,
+    a.student_last_name,
+    a.created_at,
+    a.updated_at,
+    a.team_id,
+    t.name           AS team_name,
+    t.created_at     AS team_created_at
+FROM assignments a, team t
+WHERE a.course_participation_id = ANY(@course_participation_ids::uuid[])
+  AND t.id = a.team_id
+ORDER BY a.created_at ASC;

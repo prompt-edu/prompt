5692a471bc4df31b74f1e3bce0f3e49725ece316
Josef Schmid <51161233+JGStyle@users.noreply.github.com>
2026-06-16 08:06:17 +0200

    remove pgxpool.Pool from PrivacyServiceSingleton as it is not needed

--- servers/interview/privacy/main.go
+++ servers/interview/privacy/main.go
@@ -2,15 +2,13 @@ package privacy
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/prompt-edu/prompt-sdk/promptTypes"
 	db "github.com/prompt-edu/prompt/servers/interview/db/sqlc"
 )
 
-func InitPrivacyModule(routerGroup *gin.RouterGroup, queries db.Queries, conn *pgxpool.Pool) {
-	promptTypes.RegisterPrivacyDataExportEndpoint(routerGroup, PrivacyDataExportHandler, []string{})
+func InitPrivacyModule(routerGroup *gin.RouterGroup, queries db.Queries) {
 	PrivacyServiceSingleton = &PrivacyService{
 		Queries: queries,
-		Conn:    conn,
 	}
+	promptTypes.RegisterPrivacyDataExportEndpoint(routerGroup, PrivacyDataExportHandler, []string{})
 }

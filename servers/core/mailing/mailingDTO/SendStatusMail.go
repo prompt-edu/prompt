232564ace26e13fd83d2757bd9b1d4c6270074a2
Maximilian Rapp <maximilian.rapp@tum.de>
2026-07-13 20:56:04 +0200

    `Application`: Allow sending accept/reject mails to selected students

--- servers/core/mailing/mailingDTO/SendStatusMail.go
+++ servers/core/mailing/mailingDTO/SendStatusMail.go
@@ -1,7 +1,13 @@
 package mailingDTO
 
-import db "github.com/prompt-edu/prompt/servers/core/db/sqlc"
+import (
+	"github.com/google/uuid"
+	db "github.com/prompt-edu/prompt/servers/core/db/sqlc"
+)
 
 type SendStatusMail struct {
 	StatusMailToBeSend db.PassStatus `json:"statusMailToBeSend"`
+	// Optional: when set, the status mail is only sent to these participants instead of all
+	// participants with the given pass status.
+	RecipientCourseParticipationIDs []uuid.UUID `json:"recipientCourseParticipationIDs"`
 }
